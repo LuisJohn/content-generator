166abb0a9b92f55040dac0647def5990584b33f9
Luis John <mnrdzjohn@gmail.com>
2024-07-28 08:30:42 +0200

    Working API Keys with good coding practice

--- lib/gemini-ai.ts
+++ lib/gemini-ai.ts
@@ -1,7 +1,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
-const apiKey = process.env.GEMINI_API_KEY as string
-const genAI = new GoogleGenerativeAI(apiKey);
+
+const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY as string);
 
 const model = genAI.getGenerativeModel({
     model: 'gemini-1.5-flash',
